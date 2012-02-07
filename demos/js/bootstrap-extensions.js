267e38923100835a995b72c7bb951790e87d458b
Mjumbe Wawatu Poe <mjumbewu@gmail.com>
2012-02-07 11:14:09 -0500

    Clean up (added some comments, wrapped in a function, used strict mode)

--- demos/js/bootstrap-extensions.js
+++ demos/js/bootstrap-extensions.js
@@ -1,75 +1,93 @@
-$.extend($.fn.twipsy.Twipsy.prototype, {
-  show: function() {
-    var $tip
-      , tp
+/* ===========================================================
+ * Modifications to the Bootstrap default behaviors.
+ * =========================================================== */
 
-    if (this.hasContent() && this.enabled) {
-      $tip = this.tip()
-      this.setContent()
+(function($) {
+  "use strict";
 
-      if (this.options.animate) {
-        $tip.addClass('fade')
-      }
-
-      $tip
-        .remove()
-        .css({ top: 0, left: 0, display: 'block' })
-        .prependTo(document.body)
-
-      this.place();
-
-      $tip
-        .addClass('in')
-    }
+  function maybeCall ( thing, ctx, args ) {
+    return typeof thing == 'function' ? thing.apply(ctx, args) : thing
   }
 
-, place: function() {
-    var pos
-      , actualWidth
-      , actualHeight
-      , placement
-      , $tip
-      , tp
+  /* ===========================================================
+   * Change the Twipsy class to separate out the placement of
+   * the tip from the showing of the tip.  This is so that we
+   * can recalculate where the tip should go without having to
+   * re-show it, since show does the whole fading out/in thing.
+   * =========================================================== */
+  $.extend($.fn.twipsy.Twipsy.prototype, {
+    show: function() {
+      var $tip
+        , tp
 
-    if (this.hasContent() && this.enabled) {
-      $tip = this.tip()
+      if (this.hasContent() && this.enabled) {
+        $tip = this.tip()
+        this.setContent()
 
-      pos = $.extend({}, this.$element.offset(), {
-        width: this.$element[0].offsetWidth || this.$element[0].getBBox().width
-      , height: this.$element[0].offsetHeight || this.$element[0].getBBox().height
-      })
+        if (this.options.animate) {
+          $tip.addClass('fade')
+        }
 
-      // Since our bubbles are fixed-position, we only want the distance from
-      // the top of the window, not the top of the document.
-      pos.top -= $(window).scrollTop();
+        $tip
+          .remove()
+          .css({ top: 0, left: 0, display: 'block' })
+          .prependTo(document.body)
 
-      actualWidth = $tip[0].offsetWidth
-      actualHeight = $tip[0].offsetHeight
+        this.place();
 
-      function maybeCall ( thing, ctx, args ) {
-        return typeof thing == 'function' ? thing.apply(ctx, args) : thing
+        $tip
+          .addClass('in')
       }
-
-      placement = maybeCall(this.options.placement, this, [ $tip[0], this.$element[0] ])
-
-      switch (placement) {
-        case 'below':
-          tp = {top: pos.top + pos.height + this.options.offset, left: pos.left + pos.width / 2 - actualWidth / 2}
-          break
-        case 'above':
-          tp = {top: pos.top - actualHeight - this.options.offset, left: pos.left + pos.width / 2 - actualWidth / 2}
-          break
-        case 'left':
-          tp = {top: pos.top + pos.height / 2 - actualHeight / 2, left: pos.left - actualWidth - this.options.offset}
-          break
-        case 'right':
-          tp = {top: pos.top + pos.height / 2 - actualHeight / 2, left: pos.left + pos.width + this.options.offset}
-          break
-      }
-
-      $tip
-        .css(tp)
-        .addClass(placement)
     }
-  }
-});
+
+  , place: function() {
+      var pos
+        , actualWidth
+        , actualHeight
+        , placement
+        , $tip
+        , tp
+
+      if (this.hasContent() && this.enabled) {
+        $tip = this.tip()
+
+        // Check for getBBox if out element doesn't have offsetWidth/Height.
+        // Only HTML elements have offsetWidth/Height; SVG elements' equivalent
+        // is the bounding box width/height.
+        pos = $.extend({}, this.$element.offset(), {
+          width: this.$element[0].offsetWidth || this.$element[0].getBBox().width
+        , height: this.$element[0].offsetHeight || this.$element[0].getBBox().height
+        })
+
+        // Since our bubbles are fixed-position, we only want the distance from
+        // the top of the window, not the top of the document.
+        pos.top -= $(window).scrollTop();
+
+        actualWidth = $tip[0].offsetWidth
+        actualHeight = $tip[0].offsetHeight
+
+        placement = maybeCall(this.options.placement, this, [ $tip[0], this.$element[0] ])
+
+        switch (placement) {
+          case 'below':
+            tp = {top: pos.top + pos.height + this.options.offset, left: pos.left + pos.width / 2 - actualWidth / 2}
+            break
+          case 'above':
+            tp = {top: pos.top - actualHeight - this.options.offset, left: pos.left + pos.width / 2 - actualWidth / 2}
+            break
+          case 'left':
+            tp = {top: pos.top + pos.height / 2 - actualHeight / 2, left: pos.left - actualWidth - this.options.offset}
+            break
+          case 'right':
+            tp = {top: pos.top + pos.height / 2 - actualHeight / 2, left: pos.left + pos.width + this.options.offset}
+            break
+        }
+
+        $tip
+          .css(tp)
+          .addClass(placement)
+      }
+    }
+  });
+
+})(jQuery);
